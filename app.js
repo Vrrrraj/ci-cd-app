e8c8d7ce68a5eec1ce53db14e49547513656436a
Vraj Patel <142654418+Vrrrraj@users.noreply.github.com>
2026-03-25 10:32:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello DevOps CI/CD 🚀');
+  res.send('Hello CI/CD Working 🚀');
 });
 
 app.listen(3000, () => {
